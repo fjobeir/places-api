a8043eeb3f388f866c9ba6941dc368747b867e12
Feras Jobeir <fjobeir@gmail.com>
2022-05-19 13:26:02 +0300

    transform places inside category

--- transformers/categoryTransformers.js
+++ transformers/categoryTransformers.js
@@ -1,5 +1,10 @@
+var { placesTransformer } = require('../transformers/placeTransformers')
+
 const categoryTransformer = (category) => {
     category.icon = `${process.env.URL + process.env.UPLOADS +  category.icon}`
+    if (category?.Places) {
+        category.Places = placesTransformer(category.Places)
+    }
     return category
 }
 const categoriesTransformer = (arrayOfCategories) => {
